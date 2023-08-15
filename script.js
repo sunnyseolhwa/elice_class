96a0f72e53629e465146a60e5e331f23eba8d230
seol <seolhwa7898@gmail.com>
2023-08-15 14:02:56 +0900

    스크립트 첫 수업

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('hello javascript');
+
+//변수 선언과 함수 작성
+// var let const 
+
+var age = 25;
+let year = 2023;
+const point = 250;
+
+// 데이터 타입 : 문자, 숫자 / int, float 
+// Number = 1,2,3,4,5 숫자 
+// String = 'hello world' 문자열 
+// Boolean = true, false 
+// Null, Undefinde, Object
+
+const pass = true;
+
+if(pass){
+    console.log('hello');
+}
